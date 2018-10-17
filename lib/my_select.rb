805c51f05ee0e0d7744beb788f518ae2d08466b2
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-17 02:09:14 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ i=0
+ out=[]
+ while i<collection.length
+    if yield collection[i]
+    i=+1
+  end
+  out
+end
+
+my_select([1,2,3,4,5])do |num|
+  puts num
 end
